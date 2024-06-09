8687dae172f536f1e29d095d127e1bda58a85b2d
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-06-09 04:55:05 +0300

    breadth first working

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit=[self.root]
+    while nodes_to_visit:
+      node=nodes_to_visit.pop(0)
+      if node['id'] == id:
+        return node
+      if'children' in node:
+        nodes_to_visit.extend(node['children'])
+    return None
+
